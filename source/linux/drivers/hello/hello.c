916480071edf5df87e66010820f45bea6dfde6e8
Ngoc Ho <phuongngoc31@gmail.com>
2024-07-14 14:49:26 +0700

    Add parameters to Greeting module
    Add howmany and whom parameters to Greeting module,
    display number of greetings and recipient message.
    
    Test case:
    ~# modinfo /lib/modules/6.7.12/hello.ko
    filename:       /lib/modules/6.7.12/hello.ko
    author:         Ngoc Ho
    description:    Greeting module
    license:        GPL
    parm:           howmany:Number of greetings
    parm:           whom:Recipient of the hello message
    depends:
    vermagic:       6.7.12 SMP mod_unload ARMv7 p2v8
    ~# insmod /lib/modules/6.7.12/hello.ko howmany=3 whom=ngoc
    [ 3744.908321] system name = Linux
    [ 3744.911597] node name   = buildroot
    [ 3744.915131] release     = 6.7.12
    [ 3744.918621] machine     = armv7l
    [ 3744.921902] Hello World. You are currently using Linux #1 SMP Sun Jul 14 12:34:32 +07 2024.
    [ 3744.930459] (0) Hello, ngoc
    [ 3744.933303] (1) Hello, ngoc
    [ 3744.936190] (2) Hello, ngoc
    ~# rmmod hello
    [ 3754.212651] Good bye!
    
    Signed-off-by: Ngoc Ho <phuongngoc31@gmail.com>

--- source/linux/drivers/hello/hello.c
+++ source/linux/drivers/hello/hello.c
@@ -5,14 +5,27 @@
 #include <linux/kernel.h>
 #include <linux/utsname.h>
 
+static char *whom = "world";
+module_param(whom, charp, 0644);
+MODULE_PARM_DESC(whom, "Recipient of the hello message");
+
+static int howmany = 1;
+module_param(howmany, int, 0644);
+MODULE_PARM_DESC(howmany, "Number of greetings");
+
 static int __init hello_init(void)
 {
+    int i;
+
     pr_info("system name = %s\n", utsname()->sysname);
     pr_info("node name   = %s\n", utsname()->nodename);
     pr_info("release     = %s\n", utsname()->release);
     pr_info("machine     = %s\n", utsname()->machine);
 
     pr_alert("Hello World. You are currently using Linux %s.\n", utsname()->version);
+
+    for (i = 0; i < howmany; i++)
+        pr_alert("(%d) Hello, %s\n", i, whom);
 
     return 0;
 }
